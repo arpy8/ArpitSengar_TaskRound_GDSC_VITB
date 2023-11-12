b467418f3f64b3a433eb971de3199bb06e5d5efa
Arpit <arpitsengar99@gmail.com>
2023-11-12 16:12:23 +0530

    modified sonnets.txt path

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Shakespeare’s Sonnets", page_icon="📔")
 
-shakespeare_sonnets = open("./data/Sonnets.txt", "r").readlines()
+shakespeare_sonnets = open("./Data/Sonnets.txt", "r").readlines()
 shakespeare_text = [text.rstrip("\n") for text in shakespeare_sonnets] 
 
 ngram_model = {}
